cffd1d59ef77f718494a14c9bda09d630397bdb7
Gabriel <gabriel.luthun@gmail.com>
2024-05-21 16:37:50 +0200

    feat:add_string_without_space_kata

---
+++ 8-kyu/no-space.js
@@ -0,0 +1,8 @@
+//Write a function that removes the spaces from the string, then return the resultant string.
+
+function noSpace(x) {
+    return x.split(' ').join('');
+}
+
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')); // '8j8mBliB8gimjB8B8jlB'
